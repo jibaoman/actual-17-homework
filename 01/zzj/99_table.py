5c74968f157e81eda14e335bf3004d97d5a59de9
root <root@91.(none)>
2017-11-27 10:03:21 +0800

    first commit:Ezail

--- 01/zzj/99_table.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
-for i in range(1,10):
-    for j in range(1,10):
-        if i >= j:
-            print '%s x %s = %s\t' %(i,j,i*j),
-    print '\n'
